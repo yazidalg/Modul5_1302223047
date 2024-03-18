111b124cdb3f2705ac98167169ac259599ffd4e2
unknown <yazid.al2418@gmail.com>
2024-03-18 07:20:57 +0700

    add classs Penjumlahan with 3 number

--- MOD5_1302223047/Program.cs
+++ MOD5_1302223047/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Penjumlahan
+{
+    public static void JumlahTigaAngka<T>(T prm1, T prm2, T prm3)
+    {
+        dynamic temp;
+        dynamic angka1 = prm1;
+        dynamic angka2 = prm2;
+        dynamic angka3 = prm3;
+        temp = angka1 + angka2 + angka3;
+        Console.WriteLine(temp);
+    }
+}
+
+public class MainProgram
+{
+    public static void Main(String[] args)
+    {
+        Penjumlahan.JumlahTigaAngka<int>(13, 02, 22);
+    }
+}
